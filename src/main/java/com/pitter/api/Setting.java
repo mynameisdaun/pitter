d1cc7af809b4cc8abe14f38a1c9f3bcfbf203d1d
mynameisdaun <daun9870.jung@gmail.com>
2022-01-16 20:50:43 +0900

    env check

--- src/main/java/com/pitter/api/Setting.java
+++ src/main/java/com/pitter/api/Setting.java
@@ -11,12 +11,16 @@ public class Setting {
     @Value("${setting.myname}")
     String myname;
 
+    @Value("${spring.datasource.url}")
+    String datasource;
+
     @GetMapping("/env")
     public String setting() {
         System.out.println("====================[DEBUG]====================");
         String profile = System.getProperty("spring.profiles.active");
         System.out.println(profile);
         System.out.println(myname);
+        System.out.println("dataSource : "+ datasource );
         return myname;
     }
 
