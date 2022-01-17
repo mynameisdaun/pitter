48aa5bc3416cfbcfe220f572b3373f00f79c0b6a
mynameisdaun <daun9870.jung@gmail.com>
2022-01-18 00:37:59 +0900

    feat SecurityConfig

--- src/main/java/com/pitter/config/auth/SecurityConfig.java
+++ src/main/java/com/pitter/config/auth/SecurityConfig.java
@@ -3,10 +3,11 @@ package com.pitter.config.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
